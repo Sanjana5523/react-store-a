8a44bfbe0704d3597e434df6b30e3a5e25d4dfc1
Sanjana5523 <ganjisanjana55@gmail.com>
2025-03-19 11:12:14 +0530

    modified

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,12 +1,12 @@
 import "./products.css"
 export default function Products() {
   const products = [
-    { id: 1, name: "Product 1", price: 30 ,url:"https://picsum.photos/id/1/200/300" },
-    { id: 2, name: "Product 2", price: 40 ,url:"https://picsum.photos/id/1/200/300"},
-    { id: 3, name: "Product 3", price: 45,url:"https://picsum.photos/id/1/200/300" },
-    { id: 4, name: "Product 4", price: 95 ,url:"https://picsum.photos/id/1/200/300"},
-    { id: 5, name: "Product 5", price: 70 ,url:"https://picsum.photos/id/1/200/300"},
-    { id: 6, name: "Product 6", price: 25 ,url:"https://picsum.photos/id/1/200/300"},
+    { id: 1, name: "Product 1", price: 30  },
+    { id: 2, name: "Product 2", price: 40 },
+    { id: 3, name: "Product 3", price: 45},
+    { id: 4, name: "Product 4", price: 95 },
+    { id: 5, name: "Product 5", price: 70 },
+    { id: 6, name: "Product 6", price: 25 },
   ];
   return (
     <div>
@@ -15,8 +15,6 @@ export default function Products() {
           <div className="App-Products-Box" key={index}>
             <h3>{value.name}</h3>
             <h4>{value.price}</h4>
-             {value.url && <img src={value.url} alt={value.name} />}
-             <br></br><br></br>
             <button className="btn">Add to Cart</button>
             </div>
         ))}
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App7 />
+    <App7/>
 );
 
 reportWebVitals();
