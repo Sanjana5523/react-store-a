c7ca99933905471dcc7697a9a55167c708e43820
Sanjana5523 <ganjisanjana55@gmail.com>
2025-03-21 12:15:29 +0530

    modified

--- src/App1.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-export default function App1(props){
-    return(
-        <div style={{backgroundColor:'yellow'}}>App1{props.name},{props.age}</div>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App7 from './App7';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App7/>
-);
-
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
